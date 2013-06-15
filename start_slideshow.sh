720e494449be39acec52b31be7bb39cbb2647a2c
CalumJEadie <calum@calumjeadie.com>
2013-06-15 09:10:10 +0000

    Updated pqi options

--- start_slideshow.sh
+++ start_slideshow.sh
@@ -3,7 +3,13 @@
 SLIDESHOW_IMG="/tmp/slideshow.jpg"
 
 # Start and restart slideshow as needed.
-until /usr/bin/pqiv -i -f -s -w $SLIDESHOW_IMG; do
+# -i no yellow box
+# -f fullscreen 
+# -s auto start slide show
+# -w watch open file for changes and reload if needed
+# -t scale images to fill screen
+# -p 4 use hyper interpolation
+until /usr/bin/pqiv -i -f -s -w -t -p 4 $SLIDESHOW_IMG; do
   echo "Slideshow crashed with exit code $?. Restarting..." >&2
   sleep 2
 done
